c1fe683f4ca510fdc00b6544c0741c07a20aa79a
Allef Frazão Santana (Garug) <allefff@gmail.com>
2021-03-16 19:57:43 -0300

    Solução de desafio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 
 const { v4: uuid } = require("uuid");
@@ -23,6 +24,8 @@ app.post("/repositories", (request, response) => {
     likes: 0,
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
@@ -30,13 +33,15 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(
+  repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
   );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
+  delete updatedRepository.likes;
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -52,7 +57,7 @@ app.delete("/repositories/:id", (request, response) => {
     (repository) => repository.id === id
   );
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -74,7 +79,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json("likes");
+  return response.json({ likes });
 });
 
 module.exports = app;
